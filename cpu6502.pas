43a54c871eb2c2b60a7f905d10ccb78ddc21e3ef
Bernd Boeckmann <bernd@boeckmann.io>
2017-02-05 21:58:14 +0100

    implement simulator loop with termination at pc addr

--- cpu6502.pas
+++ cpu6502.pas
@@ -22,8 +22,10 @@ TCpu6502 = object
   OpTbl: array[0..255] of procedure of object;
 
   procedure ExecuteNext;
+  procedure ExecuteToWithDump(pcBreak: word);
 
   function LoadIncPC : byte; inline;
+  procedure PCAddRel(const addr: byte);
 
   procedure AluADC(const m: byte); inline;
   procedure AluSBC(const m: byte); inline;
@@ -34,13 +36,25 @@ TCpu6502 = object
   procedure AluUpdateNZ(const op: byte); inline;
 
   procedure InitOpTbl;
-  procedure OpSEC;
-  procedure OpCLC;
-  procedure OpSEI;
-  procedure OpCLI;
-  procedure OpSED;
-  procedure OpCLD;
-  procedure OpCLV;
+  procedure OpBPL;    { opcode $10 - branch on PLus }
+  procedure OpCLC;    { opcode $18 - clear carry }
+  procedure OpBMI;    { opcode $30 - branch on MInus }
+  procedure OpSEC;    { opcode $38 - set carry flag }
+  procedure OpJMPabs; { opcode $4C - jump absolute }
+  procedure OpBVC;    { opcode $50 - branch if overflow clear }
+  procedure OpCLI;    { opcode $58 - clear interrupt enable flag  }
+  procedure OpADCimm; { opcode $69 - add imm to accumulator with carry }
+  procedure OpBVS;    { opcode $70 - branch if overflow set }
+  procedure OpSEI;    { opcode $78 - set interrupt enable flag }
+  procedure OpBCC;    { opcode $90 - branch if carry clear }
+  procedure OpLDAimm; { opcode $A9 - load accumulator with immediate }
+  procedure OpBCS;    { opcode $B0 - branch if carry set }
+  procedure OpCLV;    { opcode $B8 - clear overflow flag }
+  procedure OpBNE;    { opcode $D0 - branch if not equal }
+  procedure OpCLD;    { opcode $D8 - clear decimal flag }
+  procedure OpNOP;    { opcode $EA - no operation }
+  procedure OpBEQ;    { opcode $F0 - branch if equal }
+  procedure OpSED;    { opcode $F8 - set decimal flag }
 
   procedure DumpRegs;
 end;
@@ -66,13 +80,38 @@ begin
   OpTbl[opcode]();
 end;
 
+procedure TCpu6502.ExecuteToWithDump(pcBreak: word);
+var
+  opcode: byte;
+begin
+  while PC <> pcBreak do begin
+    opcode := LoadIncPC;
+    OpTbl[opcode]();
+    DumpRegs;
+  end;
+end;
 
-{--- memory access functions -------------------------------------------------}
+{--- PC and memory access functions -------------------------------------------}
 
+{ load memory from PC abs, PC <- PC + 1 }
 function TCpu6502.LoadIncPC : byte;
 begin
   LoadIncPC := LoadByte(PC);
   inc(PC);
+end;
+
+{ add signed byte to PC }
+procedure TCpu6502.PCAddRel(const addr: byte);
+var
+  tmp: byte;
+begin
+  if (addr and BIT_7) <> 0 then begin
+    { negative relative value }
+    tmp := (addr xor $FF) + 1;
+    PC := PC - tmp;
+  end else begin
+    PC := PC + addr;
+  end;
 end;
 
 
@@ -128,55 +167,168 @@ begin
   FlagN := ((op and BIT_7) <> 0);
 end;
 
-procedure TCpu6502.OpSEC;
+
+{--- opcode implementation ---------------------------------------------------}
+
+procedure TCpu6502.OpBPL;    { opcode $10 - branch on PLus }
+var
+  rel: byte;
 begin
-  FlagC := true;
+  rel := LoadIncPC;
+  if not FlagN then PCAddRel(rel);
 end;
 
-procedure TCpu6502.OpCLC;
+procedure TCpu6502.OpCLC;    { opcode $18 - clear carry flag }
 begin
   FlagC := false;
 end;
 
-procedure TCpu6502.OpSEI;
+procedure TCpu6502.OpBMI;    { opcode $30 - branch on MInus }
+var
+  rel: byte;
 begin
-  FlagI := true;
+  rel := LoadIncPC;
+  if FlagN then PCAddRel(rel);
 end;
 
-procedure TCpu6502.OpCLI;
+procedure TCpu6502.OpSEC;    { opcode $38 - set carry flag }
+begin
+  FlagC := true;
+end;
+
+procedure TCpu6502.OpJMPabs; { opcode $4C - jump absolute }
+var
+  tmpPC: word;
+begin
+  tmpPC := LoadIncPC;
+  tmpPC := tmpPC or (word(LoadIncPC()) shl 8);
+  PC := tmpPC;
+end;
+
+procedure TCpu6502.OpBVC;    { opcode $50 - branch if overflow clear }
+var
+  rel: byte;
+begin
+  rel := LoadIncPC;
+  if not FlagV then PCAddRel(rel);
+end;
+
+procedure TCpu6502.OpCLI;    { opcode $58 - clear interrupt enable flag  }
 begin
   FlagI := false;
 end;
 
-procedure TCpu6502.OpSED;
+procedure TCpu6502.OpADCimm; { opcode $69 - add immediate to accumulator with carry }
+var
+  m: byte;
 begin
-  FlagD := true;
+  m := LoadIncPC;
+  AluADC(m);
 end;
 
-procedure TCpu6502.OpCLD;
+procedure TCpu6502.OpBVS;    { opcode $70 - branch if overflow set }
+var
+  rel: byte;
 begin
-  FlagD := false;
+  rel := LoadIncPC;
+  if FlagV then PCAddRel(rel);
 end;
 
-procedure TCpu6502.OpCLV;
+procedure TCpu6502.OpSEI;    { opcode $78 - set interrupt enable flag  }
+begin
+  FlagI := true;
+end;
+
+procedure TCpu6502.OpBCC;    { opcode $90 - branch if carry clear }
+var
+  rel: byte;
+begin
+  rel := LoadIncPC;
+  if not FlagC then PCAddRel(rel);
+end;
+
+procedure TCpu6502.OpLDAimm; { opcode $A9 - load accumulator with immediate }
+begin
+  A := LoadIncPC;
+  AluUpdateNZ(A);
+end;
+
+procedure TCpu6502.OpBCS;    { opcode $B0 - branch if carry set }
+var
+  rel: byte;
+begin
+  rel := LoadIncPC;
+  if FlagC then PCAddRel(rel);
+end;
+
+procedure TCpu6502.OpCLV;    { opcode $B8 - clear overflow flag }
 begin
   FlagV := false;
 end;
 
-procedure TCpu6502.InitOpTbl;
+procedure TCpu6502.OpBNE;    { opcode $D0 - branch if not equal }
+var
+  rel: byte;
 begin
-  OpTbl[$18] := @OpCLC;  { clear carry flag }
-  OpTbl[$38] := @OpSEC;  { set carry flag }
-  OpTbl[$58] := @OpCLI;  { clear interrupt flag }
-  OpTbl[$78] := @OpSEI;  { set interrupt flag }
-  OpTbl[$B8] := @OpCLV;  { clear overflow flag }
-  OpTbl[$D8] := @OpCLD;  { clear decimal flag }
-  OpTbl[$F8] := @OpSED;	{ set decimal flag }
+  rel := LoadIncPC;
+  if not FlagZ then PCAddRel(rel);
+end;
+
+procedure TCpu6502.OpCLD;    { opcode $D8 - clear decimal flag }
+begin
+  FlagD := false;
+end;
+
+procedure TCpu6502.OpNOP;    { opcode $EA - no operation }
+begin
+end;
+
+procedure TCpu6502.OpBEQ;    { opcode $D0 - branch if equal }
+var
+  rel: byte;
+begin
+  rel := LoadIncPC;
+  if FlagZ then PCAddRel(rel);
+end;
+
+procedure TCpu6502.OpSED;    { opcode $F8 - set decimal flag }
+begin
+  FlagD := true;
+end;
+
+
+procedure TCpu6502.InitOpTbl;
+var
+  i: integer;
+begin
+  for i := low(OpTbl) to high(OpTbl) do begin
+    OpTbl[i] := @OpNOP;
+  end;
+
+  OpTbl[$10] := @OpBPL;      { branch if plus }
+  OpTbl[$18] := @OpCLC;      { clear carry flag }
+  OpTbl[$30] := @OpBMI;      { branch if minus }
+  OpTbl[$38] := @OpSEC;      { set carry flag }
+  OpTbl[$4C] := @OpJMPabs;   { jump absolute }
+  OpTbl[$50] := @OpBVC;      { branch if overflow clear }
+  OpTbl[$58] := @OpCLI;      { clear interrupt flag }
+  OpTbl[$69] := @OpADCimm;   { add immediate to accumulator with carry }
+  OpTbl[$70] := @OpBVS;      { branch if overflow set }
+  OpTbl[$78] := @OpSEI;      { set interrupt flag }
+  OpTbl[$90] := @OpBCC;      { branch if carry clear }
+  OpTbl[$A9] := @OpLDAimm;   { load accumulator with immediate }
+  OpTbl[$B0] := @OpBCS;      { branch if carry set }
+  OpTbl[$B8] := @OpCLV;      { clear overflow flag }
+  OpTbl[$D0] := @OpBNE;      { branch if not equal }
+  OpTbl[$D8] := @OpCLD;      { clear decimal flag }
+  OpTbl[$EA] := @OpNOP;      { no operation }
+  OpTbl[$F0] := @OpBEQ;      { branch if equal }
+  OpTbl[$F8] := @OpSED;	     { set decimal flag }
 end;
 
 procedure TCpu6502.DumpRegs;
 begin
-  Write(Format('A=%2.2x X=%2.2x Y=%2.2x  ', [A, X, Y]));
+  Write(Format('PC=%4.4x[%2.2x %2.2x %2.2x]  A=%2.2x X=%2.2x Y=%2.2x  ', [PC, LoadByte(PC), LoadByte(PC+1), LoadByte(PC+2), A, X, Y]));
   if FlagN then Write('N') else Write('.');
   if FlagV then Write('V') else Write('.');
   if FlagB then Write('B') else Write('.');
--- sys6502.pas
+++ sys6502.pas
@@ -1,15 +1,45 @@
 program sys6502;
 
 uses
-  cpu6502;
+  sysutils, cpu6502;
 
 var
   cpu: TCpu6502;
+  mem: array[0..$3FF] of byte;
+
+  flagTest: array[0..14] of byte = ($38, $78, $f8, $18, $58, $d8, $a9, $f9, $69, $01, $d0, $fc, $4c, $fd, $03);
+
+procedure StoreMem(const addr: word; const m: byte);
+begin
+  mem[addr] := m;
+end;
+
+function LoadMem(const addr: word) : byte;
+begin
+  LoadMem := mem[addr];
+end;
+
+procedure LoadPrgAt(const addr: word; var prg: array of byte; const len: integer);
+var
+  i: integer;
+begin
+  WriteLn(format('loading %d bytes at %4.4x', [len, addr]));
+  for i := 0 to len - 1 do mem[addr+i] := prg[i];
+end;
 
 begin
-  cpu.A := $20;
-  cpu.FlagC := true;
-  cpu.AluSBC($1f);
+  cpu.InitOpTbl;
+  cpu.LoadByte := @LoadMem;
+  cpu.StoreByte := @StoreMem;
+
+  mem[$000] := $4C; mem[$001] := $00; mem[$002] := $01;  { JMP #$100    }
+  mem[$100] := $38;                                      { SEC          }
+  mem[$101] := $B0; mem[$102] := $10;                    { BCS  #$10    }
+
+  LoadPrgAt($0200, flagTest, length(flagTest));
+  cpu.PC := $0200;
+
   cpu.DumpRegs;
+  cpu.ExecuteToWithDump($03fd);
 end.
 
